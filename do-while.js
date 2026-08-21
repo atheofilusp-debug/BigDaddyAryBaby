842ffc9c81ac4ff206c86b5156d1cbe4ffdcc429
Chelseafan2010 <atheofilusp@gmail.com>
2026-08-21 14:28:12 +0700

    Add files via upload

---
+++ do-while.js
@@ -0,0 +1,22 @@
+// let nilai = "B";
+
+// switch (nilai) {
+//     case "A":
+//         console.log("sangat Baik");
+//         break;
+//     case "B":
+//         console.log("bagus");
+//         break;
+// default:
+//     console.log("tidak diketahui");
+// }
+
+// 
+
+let rows = 5;
+let i = 1;
+do {
+    console.log(" ".repeat(rows-1)+"*".repeat(i))
+
+i++;
+}while(i <= rows);
